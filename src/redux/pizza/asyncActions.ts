f85604e615080e19f56e2fe373c1efb95f4d564c
rockn1989 <gold_100@bk.ru>
2022-06-28 22:34:10 +0300

    fix package.json

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -9,7 +9,7 @@ export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
     const { data } = await axios.get<Pizza[]>(
       `https://628ca39e3df57e983ed2f993.mockapi.io/items?p=${currentPage}&l=4&${category}&sortBy=${sortBy}&order=${orderType}${search}`
     );
-    console.log(data);
+    
     return data;
   }
 );
